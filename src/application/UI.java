f1dd63cdcabecd065ab4f187849f109be7848c61
carlosrj01 <victor.capetini@gmail.com>
2021-05-05 11:03:08 -0300

    Upgrading Board Printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,14 +68,18 @@ public class UI {
 	
 
 	public static void printBoard(ChessPiece[][] pieces) {
+		System.out.println("    A B C D E F G H");
+		System.out.println("    ---------------");
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print((8 - i) + " | ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], false);
 			}
+			System.out.print("| " + (8 - i));
 			System.out.println("");
 		}
-		System.out.println("  A B C D E F G H");
+		System.out.println("    ---------------");
+		System.out.println("    A B C D E F G H");
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
